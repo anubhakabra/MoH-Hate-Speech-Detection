d85029247217122bea33a5a490895d1f6b51862e
Arushi Sharma <40266350+arushi-08@users.noreply.github.com>
2021-05-23 18:50:02 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,15 +1,26 @@
+import os
+
 PATH = "./data"
 
 # Compute train and warmup steps from batch size
 BATCH_SIZE = 32
 LEARNING_RATE = 2e-5
 NUM_TRAIN_EPOCHS = 10.0
-WARMUP_PROPORTION = 0.005
+WARMUP_PROPORTION = 0.1
 MAX_SEQ_LENGTH = 128
+
 # Model configs
 SAVE_CHECKPOINTS_STEPS = 500
 SAVE_SUMMARY_STEPS = 100
 
 SIMILARITY_THRESHOLD = 0.70
 
-BERT_MODEL_HUB = "https://tfhub.dev/google/bert_multi_cased_L-12_H-768_A-12/1"
+# Model
+mBERT_MODULE_URL = "https://tfhub.dev/tensorflow/bert_multi_cased_L-12_H-768_A-12/4"
+MuRIL_MODULE_URL = "https://tfhub.dev/google/MuRIL/1"
+
+
+# DATA files
+TRAC1_FILE = os.path.join(PATH, "data/trac_data.csv")
+HS_FILE = os.path.join(PATH, "data/hs_data.csv")
+HOT_FILE = os.path.join(PATH, "data/hot_data.csv")
